d386f2c3b299b80e7211dba9c295a4d6286a2e49
Chris Powell <cpowell@prylis.com>
2013-08-19 11:09:05 -0700

    Initial Commit

---
+++ fam/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fam
+//
+//  Created by Chris Powell on 8/19/13.
+//  Copyright (c) 2013 Prylis Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
