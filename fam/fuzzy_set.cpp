4a07b3f2b3d1aee0740db105cecf918a89fada27
Chris Powell <cpowell@prylis.com>
2013-08-23 14:57:47 -0700

    Licensing notices

--- fam/fuzzy_set.cpp
+++ fam/fuzzy_set.cpp
@@ -5,4 +5,8 @@
 //  Created by Chris Powell on 8/19/13.
 //  Copyright (c) 2013 Prylis Inc. All rights reserved.
 //
+//  This file is part of The C++ Fuzzy Associative Memory
+//  http://github.com/cpowell/fuzzy-associative-memory-c++
+//  You can redistribute and/or modify this software only in accordance with
+//  the terms found in the "LICENSE" file included with the library.
 
--- fam/linguistic_variable.h
+++ fam/linguistic_variable.h
@@ -5,6 +5,11 @@
 //  Created by Chris Powell on 8/19/13.
 //  Copyright (c) 2013 Prylis Inc. All rights reserved.
 //
+//  This file is part of The C++ Fuzzy Associative Memory
+//  http://github.com/cpowell/fuzzy-associative-memory-c++
+//  You can redistribute and/or modify this software only in accordance with
+//  the terms found in the "LICENSE" file included with the library.
+//
 
 #ifndef __fam__linguistic_variable__
 #define __fam__linguistic_variable__
