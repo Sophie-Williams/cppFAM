75779de812e480caef1ccab61998b1896fe3c868
Chris Powell <cpowell@prylis.com>
2013-08-23 15:24:53 -0700

    Correct github link in headers.

--- fam/fuzzy_set.cpp
+++ fam/fuzzy_set.cpp
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Prylis Inc. All rights reserved.
 //
 //  This file is part of The C++ Fuzzy Associative Memory
-//  http://github.com/cpowell/fuzzy-associative-memory-c++
+//  http://github.com/cpowell/fuzzy-associative-memory-cplusplus
 //  You can redistribute and/or modify this software only in accordance with
 //  the terms found in the "LICENSE" file included with the library.
 
