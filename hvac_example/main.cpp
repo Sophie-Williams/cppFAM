a26dbbe36cc3160c10e5d4cca1336deafb4f1cd0
Chris Powell <cpowell@prylis.com>
2013-09-12 08:03:53 -0700

    Got LV to take unique_ptr sets.

--- hvac_example/main.cpp
+++ hvac_example/main.cpp
@@ -21,6 +21,7 @@
 
 using std::cout;
 using std::shared_ptr;
+using std::unique_ptr;
 using std::make_shared;
 
 using namespace fuzzy;
@@ -31,56 +32,75 @@ int main(int argc, const char * argv[])
 
     auto temperature_in = make_shared<LinguisticVariable>("room temperature");
 
-    auto cold = make_shared<Trapezoid>(40, 40, 40, 50);
-    auto cool = make_shared<Triangle>(45, 55, 65);
-    auto ok   = make_shared<Triangle>(60, 65, 70);
-    auto warm = make_shared<Triangle>(65, 75, 85);
-    auto hot  = make_shared<Trapezoid>(80, 90, 90, 90);
+//    Trapezoid cold {40, 40, 40, 50};
+//    Triangle  cool {45, 55, 65};
+//    Triangle  ok   {60, 65, 70};
+//    Triangle  warm {65, 75, 85};
+//    Trapezoid hot  {80, 90, 90, 90};
 
-    temperature_in->addSet(cold);
-    temperature_in->addSet(cool);
-    temperature_in->addSet(ok);
-    temperature_in->addSet(warm);
-    temperature_in->addSet(hot);
+//    auto cold = make_shared<Trapezoid>(40, 40, 40, 50);
+//    auto cool = make_shared<Triangle>(45, 55, 65);
+//    auto ok   = make_shared<Triangle>(60, 65, 70);
+//    auto warm = make_shared<Triangle>(65, 75, 85);
+//    auto hot  = make_shared<Trapezoid>(80, 90, 90, 90);
+
+    unique_ptr<FuzzySet> cold( new Trapezoid(40,40,40,50));
+    unique_ptr<FuzzySet> cool( new Triangle(45, 55, 65));
+    unique_ptr<FuzzySet> ok( new Triangle(60, 65, 70));
+    unique_ptr<FuzzySet> warm( new Triangle(65, 75, 85));
+    unique_ptr<FuzzySet> hot( new Trapezoid(80, 90, 90, 90));
+
+    temperature_in->addSet(std::move(cold));
+    temperature_in->addSet(std::move(cool));
+    temperature_in->addSet(std::move(ok));
+    temperature_in->addSet(std::move(warm));
+    temperature_in->addSet(std::move(hot));
+
 
     auto fan_speed = make_shared<LinguisticVariable>("fan speed");
 
-    auto stop  = make_shared<Triangle>(-30, 0, 30);
-    auto slow  = make_shared<Triangle>(10, 30, 50);
-    auto med   = make_shared<Triangle>(40, 50, 60);
-    auto fast  = make_shared<Triangle>(50, 70, 90);
-    auto blast = make_shared<Triangle>(70, 100, 130);
+    unique_ptr<FuzzySet> stop(new Triangle(-30, 0, 30));
+    unique_ptr<FuzzySet> slow(new Triangle(10, 30, 50));
+    unique_ptr<FuzzySet> med(new Triangle(40, 50, 60));
+    unique_ptr<FuzzySet> fast(new Triangle(50, 70, 90));
+    unique_ptr<FuzzySet> blast(new Triangle(70, 100, 130));
 
-    fan_speed->addSet(stop);
-    fan_speed->addSet(slow);
-    fan_speed->addSet(med);
-    fan_speed->addSet(fast);
-    fan_speed->addSet(blast);
+//    auto stop  = make_shared<Triangle>(-30, 0, 30);
+//    auto slow  = make_shared<Triangle>(10, 30, 50);
+//    auto med   = make_shared<Triangle>(40, 50, 60);
+//    auto fast  = make_shared<Triangle>(50, 70, 90);
+//    auto blast = make_shared<Triangle>(70, 100, 130);
 
-    auto system = make_shared<RuleSet>("HVAC control", "larsen");
+    fan_speed->addSet(std::move(stop));
+    fan_speed->addSet(std::move(slow));
+    fan_speed->addSet(std::move(med));
+    fan_speed->addSet(std::move(fast));
+    fan_speed->addSet(std::move(blast));
 
-    auto r1 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{cold}, "", stop,  "if room is cold, fan motor stops");
-    auto r2 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{cool}, "", slow,  "if room is cool, fan motor is slow");
-    auto r3 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{ok},   "", med,   "if room is ok, fan motor is medium");
-    auto r4 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{warm}, "", fast,  "if room is warm, fan motor speeds up");
-    auto r5 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{hot},  "", blast, "if room is hot, fan motor runs full blast");
-
-    system->addRule(r1);
-    system->addRule(r2);
-    system->addRule(r3);
-    system->addRule(r4);
-    system->addRule(r5);
-
-    //double result;
-    for (int i=0; i<50000; ++i) {
-        for (double t=40; t<=90; ++t) {
-            system->calculate( vector<double>{t} );
-
-            //        cout << "The " << system->name() << " determines: for " << temperature_in->name();
-            //        cout << " " << t << ", the " << fan_speed->name() << " is ";
-            //        cout << result << " CFM\n";
-        }
-    }
+//    auto system = make_shared<RuleSet>("HVAC control", "larsen");
+//
+//    auto r1 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{cold}, "", stop,  "if room is cold, fan motor stops");
+//    auto r2 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{cool}, "", slow,  "if room is cool, fan motor is slow");
+//    auto r3 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{ok},   "", med,   "if room is ok, fan motor is medium");
+//    auto r4 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{warm}, "", fast,  "if room is warm, fan motor speeds up");
+//    auto r5 = make_shared<Rule>(vector<shared_ptr<FuzzySet>>{hot},  "", blast, "if room is hot, fan motor runs full blast");
+//
+//    system->addRule(r1);
+//    system->addRule(r2);
+//    system->addRule(r3);
+//    system->addRule(r4);
+//    system->addRule(r5);
+//
+//    //double result;
+//    for (int i=0; i<50000; ++i) {
+//        for (double t=40; t<=90; ++t) {
+//            system->calculate( vector<double>{t} );
+//
+//            //        cout << "The " << system->name() << " determines: for " << temperature_in->name();
+//            //        cout << " " << t << ", the " << fan_speed->name() << " is ";
+//            //        cout << result << " CFM\n";
+//        }
+//    }
 
     return 0;
 }
